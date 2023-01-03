0d00fed797ddb8b3795f757ce00f39417a225d51
Rajnikantbhay <112321572+Rajnikantbhay@users.noreply.github.com>
2023-01-03 18:09:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const readBtn = document.getElementById("all-read-btn");
 const content = document.querySelectorAll(".content")
+const notif = document.getElementById("notif");
+
 
 readBtn.addEventListener("click", () => {
+    notif.innerText = "0"
+    
     content.forEach(con => {
         con.classList.remove("active")
     })
